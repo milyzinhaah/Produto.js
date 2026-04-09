c4c3a65f035e3dc3aa63aeb2dc6356e3c00921b4
milyzinhaah <Amorim.hemily@escola.pr.gov.br>
2026-04-08 21:52:56 -0300

    Add files via upload

---
+++ produto.js
@@ -0,0 +1,8 @@
+const NomeDoProduto = "Camiseta de Algodão";
+let PrecoDoProduto = 29.99;
+const MarcaDoProduto = "MarcaX";
+PrecoDoProduto = 39.99;
+const ProdutoCompleto = NomeDoProduto + " " + MarcaDoProduto;
+
+console.log(ProdutoCompleto);
+console.log(PrecoDoProduto)
